4e40ca19ec411b23383af7cc8cee6bc40239a147
scrunts23 <scrunts23@gmail.com>
2020-05-10 20:59:25 -0500

    Final

--- module1-python-modules-packages-and-environments/setup.py
+++ module1-python-modules-packages-and-environments/setup.py
@@ -6,15 +6,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="lambdata-scrunts23.1", # the name that you will install via pip
-    version="1.2",
+    name="lambdata_ryan",  # the name that you will install via pip
+    version="1.8.1",
     author="Ryan Davidson",
     author_email="scrunts23@gmail.com",
     description="Unit 3 Module 1",
     long_description=long_description,
-    long_description_content_type="text/markdown", # required if using a md file for long desc
-    #license="MIT",
+    long_description_content_type="text/markdown",
+    # required if using a md file for long desc
+    # license="MIT",
     url="https://github.com/scrunts23",
-    #keywords="",
-    packages=find_packages() # ["my_lambdata"]
+    # keywords="",
+    packages=find_packages()  # ["my_lambdata"]
 )
